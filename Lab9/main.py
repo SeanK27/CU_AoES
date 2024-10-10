4c787e239285cea01d4ab5b0af6b690cf43b490b
SeanK27 <seankk100@gmail.com>
2024-10-10 11:20:55 -0600

    Fading LED finished

--- Lab9/main.py
+++ Lab9/main.py
@@ -1,1 +1,17 @@
 from machine import Pin, PWM
+from time import sleep
+
+pwm = PWM(Pin(13)) # Set the pin for the PWM object
+
+pwm.freq(60) # Set the frequency of the PWM signal
+
+#dim the LED over time
+while True:
+    for duty in range(65025):
+        pwm.duty_u16(duty)
+        sleep(0.00001)
+        print(duty)
+    for duty in range(65025, 0, -1):
+        pwm.duty_u16(duty)
+        sleep(0.00001)
+        print(duty)
