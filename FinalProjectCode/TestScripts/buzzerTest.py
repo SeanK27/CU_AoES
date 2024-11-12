fb5da3eb7f77fe3fd7c456e9527a9d4bc2461c91
SeanK27 <seankk100@gmail.com>
2024-11-12 11:32:37 -0700

    Edited Buzzer Test to use our buzzer library

--- FinalProjectCode/TestScripts/buzzerTest.py
+++ FinalProjectCode/TestScripts/buzzerTest.py
@@ -1,25 +1,22 @@
 import math, time
-import machine
 # load the MicroPython pulse-width-modulation module for driving hardware
 from machine import PWM, Pin, ADC
+import productionScripts.lib.buzzerLibrary as buzzerLibrary
+
+# Pin objects
+ADCPin = Pin(27)
 
 # ADC objects
-batteryADC = ADC(Pin(32))
+batteryADC = ADC(ADCPin)
 
 # PWM objects
-buzzer = PWM(Pin(13))
-buzzer.freq(440)            # 440 Hz
-buzzer.duty_u16(30000)      # 100% duty cycle
+# buzzer = PWM(Pin(13))
+# buzzer.freq(440)            # 440 Hz
+# buzzer.duty_u16(30000)      # 100% duty cycle
 
 
 while True:
     batteryVoltage = batteryADC.read_u16() * 3.3 / 65535
     print("Battery Voltage: ", batteryVoltage)
 
-    # A siren from 1500Hz to 1980Hz and back
-    for freq in range(1500, 1980, 25):
-        buzzer.freq(freq)
-        time.sleep(0.01)
-    for freq in range(1980, 1500, -25):
-        buzzer.freq(freq)
-        time.sleep(0.01)
+    buzzerLibrary.buzzerOn(Pin(12), 440, 30000)
